a47fda8c0893a27a9da31df8736b011b53f4c097
Payton Virgil <paytonvirgil8@gmail.com>
2026-09-13 20:11:02 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,26 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Asset Dynamics",
-  description: "A computational modeling laboratory for systems, calculus, optimization, and valuation."
+  description:
+    "A computational modeling laboratory for systems, calculus, optimization, and valuation.",
+
+  icons: {
+    icon: [
+      {
+        url: "/Asset-Dynamics.png",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/Asset-Dynamics.png",
+    apple: "/Asset-Dynamics.png",
+  },
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
